6d0f7083cd94a529bcdd09a962866229875b5dd2
Matt Fava <matt.b.fava@gmail.com>
2020-08-13 18:20:52 -0400

    remove

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,9 +2,7 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get("/",function(req, res){
-    res.redirect("/burgers");
-});
+
 
 router.post("/burgers/create",function(req,res){
     burger.create(req.body.burger, function(result){
