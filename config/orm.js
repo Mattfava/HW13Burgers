15fc0248ecec772beb13764a436b8a4deeda2f80
Matt Fava <matt.b.fava@gmail.com>
2020-08-13 18:16:21 -0400

    commit

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,48 @@
 var connection = require("./connection.js");
+
+function questionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
+function pushToDB(obj){
+    var arr = [];
+
+    for(var key in obj){
+        arr.push(key + "=" + obj[key]);
+    }
+    return arr.toString();
+}
+
+var orm ={
+    all: function(table, cb){
+        var tableQuery = "SELECT * FROM "+ table +";";
+        connection.query(tableQuery, function(err,results){
+            cb(results);
+        })
+    },
+
+
+    create: function(table, columns, values, cb){
+        var dbString = "INSERT INTO " + table +"("+columns.toString() +") VALUES ("+questionMarks(values.length)+")";
+      
+
+        connection.query(dbString, values, function(err, res){
+            cb(res);
+        });
+    },
+    update: function(table, values, condition, cb){
+        var dbString2 = "UPDATE " + table +" SET "+pushToDB(values)+" WHERE "+condition;
+
+        connection.query(dbString2, function(err, res){
+            cb(res);
+        });
+    }
+};
+
+module.exports = orm;
---
+++ controllers/burgerController.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/",function(req, res){
+    res.redirect("/burgers");
+});
+
+router.post("/burgers/create",function(req,res){
+    burger.create(req.body.burger, function(result){
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/:id",function(req,res){
+    burger.update(req.params.id, function(result){
+        res.sendStatus(200);
+    });
+});
+
+router.get("/burgers",function(req, res){
+    burger.all(function(data){
+        res.render("index",{burgerData: data});
+    });
+});
+
+module.exports = router;
